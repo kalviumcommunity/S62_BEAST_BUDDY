8268caf58363a6b8a27a5add871b2e0f7c571cf6
R John Robert <rjohn.robert@kalvium.community>
2025-01-20 14:46:19 +0530

    Mongo Atlas Done

---
+++ BackEnd/src/DB/database.js
@@ -0,0 +1,19 @@
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({
+        path: "./config/.env",
+    });
+  }
+  
+  const mongoose = require("mongoose")
+  const connectDatabase = async () => {
+    console.log(process.env.DB_URL);
+    return mongoose.connect(process.env.DB_URL)
+      .then((data) => {
+        console.log(
+          `Database is connected Successfully: ${data.connection.host}`
+        );
+      })
+      .catch((err) => console.log("Database connection Failed...", err));
+  };
+  
+  module.exports = connectDatabase;
