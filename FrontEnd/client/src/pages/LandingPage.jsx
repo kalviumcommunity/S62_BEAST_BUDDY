fdb7350716f38203a0b9fc3b93d128876187ae2e
R John Robert <rjohn.robert@kalvium.community>
2025-01-29 11:41:22 +0530

    Set Up React App

---
+++ FrontEnd/client/src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+function LandingPage() {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-start h-screen pt-10">
+          <h1 className="text-4xl font-bold text-blue-900">BEAST BUDDY</h1>
+          <h1 className="text-2xl font-bold text-blue-600">Unleash Your Inner Beast!</h1>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
